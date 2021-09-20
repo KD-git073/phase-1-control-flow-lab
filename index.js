b62750c8449e2449adee0750d04226d9e4969003
Kevin Digan <kd073@hotmail.com>
2021-09-20 19:50:31 -0400

    commit tree 4f97e10ce59a8640a3a954d89b6c15b29b4353c9

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+let scootFeet = 200;
+function scuberGreetingForFeet(scootFeet) {
+  if (scootFeet <= 400)  {
+  return 'This one is on me!'
+  } else if  (scootFeet>= 2000 && scootFeet <= 2499) {
+  return 'I will gladly take your thirty bucks.'
+  } else if (scootFeet >= 2500) {
+  return ('No can do.')
+  }
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return (city == 'NYC' ? "Ok, sounds good." : "No go.");
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+//function switchOnCharmFromTip(){
+  function switchOnCharmFromTip (tip){
+    switch(tip){
+      case 'generous' :
+        return 'Thank you so much.';
+      case 'not as generous' :
+        return 'Thank you.';
+      default: return 'Bye.';   
+    }
+  }
+//}
